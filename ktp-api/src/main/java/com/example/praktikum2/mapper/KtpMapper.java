cb6ab3ee6a164e23ee0e2d869fad89f39760a6a4
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-03-12 13:20:23 +0700

    tambah fungsi pemetaan data (mapToEntity & mapToDto) untuk KTP

--- ktp-api/src/main/java/com/example/praktikum2/mapper/KtpMapper.java
+++ ktp-api/src/main/java/com/example/praktikum2/mapper/KtpMapper.java
@@ -1,4 +1,31 @@
 package com.example.praktikum2.mapper;
 
-public class KtpMapper {
+import com.example.praktikum2.model.dto.KtpAddRequest;
+import com.example.praktikum2.model.dto.KtpDto;
+import com.example.praktikum2.model.entity.Ktp;
+
+public interface KtpMapper {
+
+    // Dari Input Web (Request) ke Database (Entity)
+    static Ktp mapToEntity(KtpAddRequest request) {
+        Ktp ktp = new Ktp();
+        ktp.setNomorKtp(request.getNomorKtp());
+        ktp.setNamaLengkap(request.getNamaLengkap());
+        ktp.setAlamat(request.getAlamat());
+        ktp.setTanggalLahir(request.getTanggalLahir());
+        ktp.setJenisKelamin(request.getJenisKelamin());
+        return ktp;
+    }
+
+    // Dari Database (Entity) ke Balikan Web (DTO)
+    static KtpDto mapToDto(Ktp ktp) {
+        KtpDto dto = new KtpDto();
+        dto.setId(ktp.getId());
+        dto.setNomorKtp(ktp.getNomorKtp());
+        dto.setNamaLengkap(ktp.getNamaLengkap());
+        dto.setAlamat(ktp.getAlamat());
+        dto.setTanggalLahir(ktp.getTanggalLahir());
+        dto.setJenisKelamin(ktp.getJenisKelamin());
+        return dto;
+    }
 }
